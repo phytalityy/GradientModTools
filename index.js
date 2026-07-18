e0ef251b68f90b1567319422152d0082222925fd
phytalityy <phytalityy@gmail.com>
2026-07-18 00:52:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-(function () {
+(function (plugin, metro, patcher, storage, ui) {
+    "use strict";
 
-    console.log("Gradient Mod Tools loaded");
+    const React = metro.common.React;
+    const { Forms } = ui.components;
+    const {
+        FormRow,
+        FormDivider
+    } = Forms;
 
-    return {
-        onUnload() {
-            console.log("Gradient Mod Tools unloaded");
-        }
+    function Settings() {
+        return React.createElement(
+            React.Fragment,
+            null,
+
+            React.createElement(FormRow, {
+                label: "Gradient Mod Tools",
+                subLabel: "Plugin loaded successfully ✅"
+            }),
+
+            React.createElement(FormDivider),
+
+            React.createElement(FormRow, {
+                label: "Current Test Role",
+                subLabel: "Lead Moderator"
+            }),
+
+            React.createElement(FormRow, {
+                label: "Permissions",
+                subLabel: "Warn • Timeout • Jail • Ban"
+            })
+        );
+    }
+
+    plugin.settings = Settings;
+
+    console.log("[Gradient] Plugin Loaded");
+
+    plugin.onUnload = () => {
+        console.log("[Gradient] Plugin Unloaded");
     };
 
-})();
+    return plugin;
+
+})(
+    {},
+    vendetta.metro,
+    vendetta.patcher,
+    vendetta.storage,
+    vendetta.ui
+);
