09dbc784cfa88990f07312177b8193826ee8ff04
phytalityy <phytalityy@gmail.com>
2026-07-17 22:17:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
 (function () {
-    const React = vendetta.metro.common.React;
 
-    function createOverlay() {
-
-        const box = document.createElement("div");
-
-        box.id = "gradient-role-display";
-
-        box.innerHTML = `
-        <div style="
-            position:fixed;
-            top:20px;
-            right:20px;
-            background:#111;
-            color:white;
-            padding:15px;
-            border-radius:12px;
-            z-index:999999;
-            font-family:sans-serif;
-        ">
-            🛡 Gradient Mod Tools
-            <br><br>
-            Role:
-            <b>Lead Moderator</b>
-
-            <br><br>
-
-            Status:
-            ✅ Loaded
-        </div>
-        `;
-
-        document.body.appendChild(box);
-    }
-
-
-    createOverlay();
-
+    console.log("Gradient Mod Tools loaded");
 
     return {
         onUnload() {
-
-            const box =
-            document.getElementById(
-                "gradient-role-display"
-            );
-
-            if(box)
-                box.remove();
+            console.log("Gradient Mod Tools unloaded");
         }
     };
 
 })();
-
